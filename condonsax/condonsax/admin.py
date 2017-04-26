020403e89b9f8091dc29d9570e5d71e842a6b01e
Jay Welborn <jesse.weborn@gmail.com>
2017-04-26 17:56:34 +0200

    created admin for managing event objects

--- condonsax/condonsax/admin.py
+++ condonsax/condonsax/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
+# Home Admin and Models
 from home.admin import HomeAdmin
 from home.models import HomePageText, HomePageTextFrench, HomePageTextGerman, HomePageTextSpanish, About, Contact
+
+#Events Admin and Models
+from events.admin import EventAdmin
+from events.models import Event
 
 class ChrisCondonAdmin(admin.sites.AdminSite):
     site_header = 'Chris Condon Administration'
@@ -14,3 +19,4 @@ condon_admin.register(HomePageTextGerman, HomeAdmin)
 condon_admin.register(HomePageTextSpanish, HomeAdmin)
 condon_admin.register(About)
 condon_admin.register(Contact)
+condon_admin.register(Event, EventAdmin)
--- condonsax/events/admin.py
+++ condonsax/events/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import Event
+
 # Register your models here.
+class EventAdmin(admin.ModelAdmin)
+    """
+    Admin Page class for managing Events
+    """
+    fieldsets = [
+        ('Title', {'fields': ['title', 'slug']}),
+        ('Event and Publication Dates', {'fields': ['event_date', 'pub_date']}),
+        ('Image', {'fields': ['event_image']}),
+    ]
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'pub_date')
+    list_filter = ['pub_date']
